7bf0451b29a5b9bf7ec88d2b74e2eff3aea5a210
jj shih <micjoe@gmail.com>
2022-01-14 09:30:04 +0800

    init commit

---
+++ lib/src/main/java/com/tom/lib/HelloWorld.kt
@@ -0,0 +1,10 @@
+package com.tom.lib
+fun main() {
+    println("helloworld!!")
+    val w=65f
+    val h=1.7f
+    val bmi=w/(h*h)
+    println ("你的BMI 是 $bmi ")
+}
+class HelloWorld {
+}
---
+++ lib/src/main/java/com/tom/lib/person.kt
@@ -0,0 +1,17 @@
+package com.tom.lib
+
+fun main() {
+    val p = Person()
+    p.weight = 65f
+    p.height = 1.7f
+    println ("您的BMI是 ${p.getBmi()}")
+}
+
+class Person {
+    var weight: Float = 0f
+    var height: Float = 0f
+    fun getBmi() : Float {
+        val bmi = weight / (height * height)
+        return  bmi
+    }
+}
